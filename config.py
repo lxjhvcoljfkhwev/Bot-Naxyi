88fc5f5f639b57beb30a9d69391009fc67a8b574
lxjhvcoljfkhwev <venusstunning@gmail.com>
2025-05-11 17:13:46 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "8199727866:AAHoJ0h408z2DryeraLbyB2ehqG1HSfAsd4"  # токен ботика
+CHANNEL_URL = "https://t.me/+K_ZAakrVJhY5ZTI6"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002631465941"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002273700229"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/BotsmanNazar"
+ADMIN_ID = 7997845416 # Ваш айди без скобочек
+PROMO = "MYCASH777" # промокод
